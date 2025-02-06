ba7cec964f64ca1deaabbd7c0056f4bc1404b689
Wilter Lucas <lucasmotaoficial2018@outlook.com>
2025-02-06 16:03:29 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,6 @@
 package boardgame;
 
+//TABULEIRO
 public class Board {
 	
 	private int rows;
@@ -22,6 +23,10 @@ public class Board {
 	public void setColumns(int columns) {
 		this.columns = columns;
 	}
-	
-	
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
+	public Piece piece(Position position) {
+		return pieces [position.getRow()][position.getColumn()];
+	}
 }
